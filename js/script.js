6ac62c1892f61def5a98c046c5d77b7d6c3768e2
sushmithareddy10 <sushmithareddy1098@gmail.com>
2020-09-29 15:34:38 +0530

    made changes

--- js/script.js
+++ js/script.js
@@ -15,21 +15,28 @@ $('.slider').slick({
       }
     },
     {
+      breakpoint: 772,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        arrows: false
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
